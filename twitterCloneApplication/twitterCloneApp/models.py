77dfac4df1b39ffd17ddc46e83800c59895f67a3
Bob Hunt <bob.hunt.email@gmail.com>
2019-03-18 11:26:39 -0400

    Created forms.py, models.py, views.py. Created empty models for TwitterUser, Tweet & Notification.

---
+++ twitterCloneApplication/twitterCloneApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+def TwitterUser(models.Model):
+    pass
+
+def Tweet(models.Model):
+    pass
+
+def Notification(models.Model):
+    pass
